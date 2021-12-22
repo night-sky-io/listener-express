a31c22909cf02c3b02d460e451e2e82d01071eda
Joe-Brady <joebradydev@gmail.com>
2021-12-22 17:36:32 +0000

    Fix TS errors

---
+++ middleware-express/jest.config.d.ts
@@ -0,0 +1,8 @@
+declare const _default: {
+    clearMocks: boolean;
+    collectCoverage: boolean;
+    coverageDirectory: string;
+    coverageProvider: string;
+    preset: string;
+};
+export default _default;
