58b34a622c47e56ea423d71c85429d3bc466401e
Joe-Brady <joebradydev@gmail.com>
2021-12-23 22:13:03 +0000

    Add headers & query, bypass OPTIONS calls

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,84 @@
 import { RequestHandler } from "express";
 import axios from "axios";
+import { IncomingHttpHeaders } from "http";
+import { ParsedQs } from "qs";
 
 export interface ListenerConfig {
   satelliteHost: string;
 }
 
+export interface RequestData {
+  path: string;
+  query: ParsedQs;
+  method: string;
+  headers: IncomingHttpHeaders;
+}
+
+export interface ResponseData {
+  body: any;
+}
+
 export interface SatellitePostBody {
-  req: {
-    path: string;
-    method: string;
-  };
-  res: {
-    body: any;
-  };
+  req: RequestData;
+  res: ResponseData;
 }
 
 const listener =
   ({ satelliteHost }: ListenerConfig): RequestHandler =>
   (req, res, next) => {
-    const satelliteRequestsUrl = `${satelliteHost}/requests`;
+    if (
+      ["GET", "HEAD", "POST", "PUT", "DELETE", "PATCH"].includes(req.method)
+    ) {
+      const satelliteRequestsUrl = `${satelliteHost}/requests`;
+      const requestData: RequestData = {
+        method: req.method,
+        path: req.baseUrl + req.path,
+        query: req.query,
+        headers: req.headers,
+      };
 
-    const originalSend = res.send.bind(res);
-    res.send = function (data) {
-      if (typeof data === "string") {
+      const originalSend = res.send.bind(res);
+
+      res.send = function (data) {
+        if (typeof data === "string") {
+          const satellitePostBody: SatellitePostBody = {
+            req: requestData,
+            res: { body: data },
+          };
+
+          axios.post(satelliteRequestsUrl, satellitePostBody);
+        }
+
+        return originalSend.apply(this, arguments as unknown as [body?: any]);
+      };
+
+      const originalEnd = res.end.bind(res);
+
+      res.end = function (arg1?: Function | any) {
+        let body: any;
+        if (typeof arg1 !== "function") {
+          body = arg1;
+        }
+
         const satellitePostBody: SatellitePostBody = {
-          req: { path: req.path, method: req.method },
-          res: { body: data },
+          req: requestData,
+          res: { body },
         };
 
         axios.post(satelliteRequestsUrl, satellitePostBody);
-      }
 
-      return originalSend.apply(this, arguments as unknown as [body?: any]);
-    };
-
-    const originalEnd = res.end.bind(res);
-
-    res.end = function (arg1?: Function | any) {
-      let body: any;
-      if (typeof arg1 !== "function") {
-        body = arg1;
-      }
-
-      const satellitePostBody: SatellitePostBody = {
-        req: { path: req.path, method: req.method },
-        res: { body },
+        return originalEnd.apply(
+          this,
+          arguments as unknown as [
+            chunk: any,
+            encoding: BufferEncoding,
+            cb?: (() => void) | undefined
+          ]
+        );
       };
 
-      axios.post(satelliteRequestsUrl, satellitePostBody);
-
-      return originalEnd.apply(
-        this,
-        arguments as unknown as [
-          chunk: any,
-          encoding: BufferEncoding,
-          cb?: (() => void) | undefined
-        ]
-      );
-    };
-
-    // TODO JMB: Add handler for res.redirect()
+      // TODO JMB: Add handler for res.redirect()
+    }
 
     next();
   };
